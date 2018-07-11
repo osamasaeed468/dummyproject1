9d18741e0522081ec81b9e06ab0ca7e06800758c
osamasaeed468 <41083963+osamasaeed468@users.noreply.github.com>
2018-07-11 22:44:24 +0500

    edit1
    edit1

--- telephone/main.c
+++ telephone/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 void regular()
-{
+{    //yo yo yoyoyyoyoyo
     //Regular service: $10.00 plus first 50 minutes are free. Charges for over 50 minutes are $0.20 per minute.
     int min,cost,n;
     printf("\nEnter mins:");
